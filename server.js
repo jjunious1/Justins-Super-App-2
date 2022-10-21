b79048b94c5090bc3d4dda42984b0edcac7ad10f
Justin Junious <justinjunious@gmail.com>
2022-10-21 12:31:13 -0400

    started building out routes to database

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const db = require('./db')
 const express = require('express')
-const routes = require('./routes')
+const Food = require('./meals/food')
+const Days = require('./meals/days')
+const Recipe = require('./meals/recipe')
 
 const PORT = process.env.PORT || 3001
 
@@ -9,8 +11,22 @@ const app = express()
 //middleware
 
 app.use(express.json())
+
 //routes
-app.use('/api', routes)
+app.get('/', (req, res) => {
+  res.json('this is working')
+})
+
+app.get('/recipes', async (req, res) => {
+  const allRecipes = await Recipe.find({})
+  res.json(allRecipes)
+})
+app.post('/food', async (req, res) => {
+  const newFood = await Food.create(req.body)
+  res.json(newFood)
+})
+
+db.on('error', console.error.bind(console, 'MongoDB connection error'))
 
 app.listen(PORT, () => {
   console.log(`Express server listening on port : ${PORT}`)
