26a51f55d8849bdccade5c0c9c0130d5c6335dd4
Justin Junious <justinjunious@gmail.com>
2022-10-21 11:24:23 -0400

    updated Schemas and server file to make controller and route files

--- server.js
+++ server.js
@@ -1,12 +1,22 @@
 const db = require('./db')
 const PORT = process.env.PORT || 3001
 const express = require('express')
+const { Food } = require('./meals')
 
 const app = express()
 
 //middleware
 
 //routes
+
+app.get('/', (req, res) => {
+  res.send('this is fucking working')
+})
+
+app.post('/food', async (req, res) => {
+  let createFood = await Food.create(req.body)
+  res.json(createFood)
+})
 
 app.listen(PORT, () => {
   console.log(`Express server listening on port : ${PORT}`)
