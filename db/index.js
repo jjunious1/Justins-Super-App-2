bbbf21fd8a8e77390522eb53d9013b50ceb55dc3
Justin Junious <justinjunious@gmail.com>
2022-10-30 16:50:14 -0400

    updated to original setup

--- db/index.js
+++ db/index.js
@@ -1,13 +1,8 @@
 const mongoose = require('mongoose')
 require('dotenv').config()
 
-let dbUrl =
-  process.env.NODE_ENV === 'production'
-    ? process.env.MONGODB_URI
-    : 'mongodb://127.0.0.1:27017/justinsdatabase'
-
 mongoose
-  .connect(dbUrl)
+  .connect(process.env.MONGODB_URI)
   .then(() => {
     console.log('Successfully connected to MongoDB.')
   })
